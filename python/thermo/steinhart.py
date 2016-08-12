76dd3e789f0d050ee216e09f4065b7d63df4512e
Dimitry Kloper <dimitry.kloper@gmail.com>
2016-08-12 23:39:32 +0300

    Add calculation of optimum sense resistor

--- python/thermo/steinhart.py
+++ python/thermo/steinhart.py
@@ -41,22 +41,72 @@ import numpy
 from rdp import rdp
 
 class Thermistor(object):
-    def __init__(self):
-        self.r0 = 100000 # Ohm
-        self.beta = 3950
-        self.t0 = 298.15 # Kelvin
-        self.r_sense = 300000 # Ohm
+    def __init__(self, r0=100000, beta=3950, t0=298.15, r_sense=132000):
+        self.r0 = r0 # Ohm
+        self.beta = beta
+        self.t0 = t0 # Kelvin
+        self.r_sense = r_sense # Ohm
 
         self.r_inf = self.r0 * numpy.e ** -(self.beta / self.t0)
 
     def temp(self, v_adc):
+        """
+        Accept ADC value from thermistor connected to ADC_IN0.
+        Return temperature in Kelvin
+        """
         return self.beta / numpy.log(
             self.r_sense / self.r_inf * (float(0xfff) / v_adc - 1)
         )
 
-    def approximation(self):
-        adc_values = numpy.arange(100, 3900)
-        return rdp(zip(adc_values, self.temp(adc_values)), epsilon=0.3)
+    def resistance(self, temp):
+        """
+        Calculate resistance of a thermistor based on temperature
+        """
+        return self.r_inf * (numpy.e ** (self.beta/temp))
+
+    def temp_approximation(self, adc_start=100, adc_stop=3900, epsilon=0.3):
+        """
+        Calculate linear approximation to temperature curve for a range of
+        ADC values.
+        """
+        adc_values = numpy.arange(adc_start, adc_stop)
+        return rdp(zip(adc_values, self.temp(adc_values)), epsilon=epsilon)
+
+class Sense(object):
+    def __init__(self,
+                 vdd=3.3,
+                 thermo_resistance_start=20000,
+                 thermo_resistance_stop=800000):
+        self.thermo_range = [thermo_resistance_start,
+                             thermo_resistance_stop]
+        self.vdd = vdd
+
+    def voltage(self, sense, thermo):
+        """
+        Calculate voltage on thermistor/sense resistor junction based on
+        their resistances
+        """
+        return float(sense)/(sense+thermo) * self.vdd
+
+    def delta(self, sense):
+        """
+        Calculate thermistor/sense voltage range depending on
+        sense resistance and required thermistor resistance ranges
+        """
+        return abs(self.voltage(sense, self.thermo_range[0]) - \
+                   self.voltage(sense, self.thermo_range[1]))
+
+    def optimum_sense(self, start, stop, step):
+        """
+        Find sense resistance in specified interval that leads to a
+        maximum voltage range in thermistor/sense junction for specified
+        thermistor resistance ranges.
+        """
+        sense_values = range(start, stop, step)
+        sense = sense_values[numpy.argmax(
+            [self.delta(float(sense)) for sense in sense_values]
+        )]
+        return (sense, [self.voltage(sense, t) for t in self.thermo_range])
 
 #
 # end of file
